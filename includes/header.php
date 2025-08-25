a6dfa2a0adc527d98078bf3cd93f95a8850ccf5b
jeanphilippe45tr <makounphilippe@gmail.com>
2025-08-25 03:19:49 +0200

    accounts erase

--- includes/header.php
+++ includes/header.php
@@ -15,8 +15,8 @@ $username = $isLoggedIn ? $_SESSION['username'] : '';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MarinePower Outboards - Premium Outboard Motors</title>
-    <link rel="stylesheet" href="<?php echo $base_url; ?>assets/css/style.css">
+    <title>PowerWave Outboards - Premium Outboard Motors</title>
+    <link rel="stylesheet" href="<?php echo $base_url; ?>/assets/css/style.css">
     <link rel="stylesheet" href="<?php echo $base_url; ?>assets/css/responsive.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
@@ -37,7 +37,7 @@ $username = $isLoggedIn ? $_SESSION['username'] : '';
                 <div class="logo">
                     <a href="<?php echo $base_url; ?>index.php">
                         <img src="<?php echo $base_url; ?>assets/images/icons/logo.png" alt="MarinePower Outboards">
-                        <span>MarinePower</span>
+                        <span>PowerWave</span>
                     </a>
                 </div>
 
